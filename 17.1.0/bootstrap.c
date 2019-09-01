e39852f7e4d91b7e7ed347649d8a34917134c188
Marko Vranic <mvranic@yahoo.com>
2019-09-01 23:14:14 +0200

    bootstap c code ready.

--- 17.1.0/bootstrap.c
+++ 17.1.0/bootstrap.c
@@ -9,17 +9,27 @@
 #endif
 
 #define AWS_EXECUTION_ENV "AWS_Lambda_apl" APL_MAJOR "_lambci"
-#define APL_PATH "/opt/nodejs/node" APL_MAJOR "/node_modules:" \
-                  "/opt/nodejs/node_modules:"                    \
-                  "/var/runtime/node_modules:"                   \
-                  "/var/runtime:"                                \
-                  "/var/task"
 #define MIN_MEM_SIZE 128
 #define ARG_BUF_SIZE 32
+#define FILENAME_BUF_SIZE 128
+
+#define DYALOG "/opt/mdyalog/17.1/64/unicode/"
+#define APL_TEXTINAPLCORE "1"
+#define MAXWS "256M"
 
 int main(void) {
+  setenv("DYALOG", DYALOG, true);
+  char wsppath[FILENAME_BUF_SIZE];
+  snprintf(wsppath, FILENAME_BUF_SIZE, "%s/ws",DYALOG);
+  setenv("WSPATH", wsppath, true);
+  setenv("TRACE_ON_ERROR", "0", true);
+  char sessionfile[FILENAME_BUF_SIZE];
+  snprintf(sessionfile, FILENAME_BUF_SIZE, "%s/default.dse",DYALOG);
+  setenv("SESSION_FILE", sessionfile,true);
+  setenv("APL_TEXTINAPLCORE", APL_TEXTINAPLCORE,true);
+  setenv("MAXWS", MAXWS, true);
+ 
   setenv("AWS_EXECUTION_ENV", AWS_EXECUTION_ENV, true);
-  setenv("APL_PATH", APL_PATH, true);
 
   const char *mem_size_str = getenv("AWS_LAMBDA_FUNCTION_MEMORY_SIZE");
   int mem_size = mem_size_str != NULL ? atoi(mem_size_str) : MIN_MEM_SIZE;
@@ -30,13 +40,12 @@ int main(void) {
   char max_old_space_size[ARG_BUF_SIZE];
   snprintf(max_old_space_size, ARG_BUF_SIZE, "--max-old-space-size=%d", mem_size * 90 / 100);
 
-  execv("/opt/bin/node", (char *[]){
-                             "node",
-                             "--expose-gc",
-                             max_semi_space_size,
-                             max_old_space_size,
-                             "/opt/init/bootstrap.apl",
-                             NULL});
+
+execv(DYALOG, (char *[]){
+                             "dyalog",
+                             "+s",
+ //                            "<$@",
+                             "/opt/bootstrap.apl"});
   perror("Could not execv");
   return EXIT_FAILURE;
 }
