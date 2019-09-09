68fde2c9c41eda4b8be644533e8553d7a926d199
Marko Vranic <mvranic@yahoo.com>
2019-09-09 10:46:37 +0200

    Updated test script.

--- 17.1.0/publish.sh
+++ 17.1.0/publish.sh
@@ -12,5 +12,5 @@ for region in $REGIONS; do
     --statement-id sid1 --action lambda:GetLayerVersion --principal '*' \
     --version-number $(aws lambda publish-layer-version --region $region --layer-name $LAYER_NAME \
       --zip-file fileb://layer.zip --cli-read-timeout 0 --cli-connect-timeout 0 \
-      --description "Node.js v${APL_VERSION} custom runtime" --query Version --output text)
+      --description "APL v${APL_VERSION} custom runtime" --query Version --output text)
 done
--- 17.1.0/test.sh
+++ 17.1.0/test.sh
@@ -8,3 +8,5 @@ cd test
 rm -f lambda.zip && zip -qyr lambda.zip index
 
 docker -H :2375 run --rm -v "$PWD":/var/task -v "$PWD"/../layer:/opt lambci/lambda:provided index.myFn
+# powersehll
+# docker run  --rm -v C:\apl-lambda\17.1.0\test:/var/task -v C:\apl-lambda\17.1.0/layer:/opt  lambci/lambda:provided  index.aplHandler
