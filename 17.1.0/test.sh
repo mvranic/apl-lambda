01509d1e3ef66b3aead3e2aaeff847ff51adac3d
Marko Vranic <mvranic@yahoo.com>
2019-09-11 09:30:20 +0200

    Not working back up for idea.

--- 17.1.0/test.sh
+++ 17.1.0/test.sh
@@ -7,7 +7,7 @@ cd test
 # Create zipfile for uploading to Lambda – we don't use this here
 rm -f lambda.zip && zip -qyr lambda.zip index
 
-docker -H :2375 run --rm -v "$PWD":/var/task -v "$PWD"/../layer:/opt lambci/lambda:provided index.aplHandler
+docker run -e APLLOG=4 --rm -v "$PWD":/var/task -v "$PWD"/../layer:/opt lambci/lambda:provided index.aplHandler
 # powersehll
-# docker run  --rm -v C:\apl-lambda\17.1.0\test:/var/task -v C:\apl-lambda\17.1.0/layer:/opt  lambci/lambda:provided  index.aplHandler
-# docker run  --rm -v C:\apl-lambda\17.1.0\test:/var/task -v C:\apl-lambda\17.1.0/layer:/opt  lambci/lambda:provided  index.aplHandler '{\"some\": \"event\"}'
+# docker run -e APLLOG=4 --rm -v C:\apl-lambda\17.1.0\test:/var/task -v C:\apl-lambda\17.1.0/layer:/opt  lambci/lambda:provided  index.aplHandler
+# docker run -e APLLOG=4 --rm -v C:\apl-lambda\17.1.0\test:/var/task -v C:\apl-lambda\17.1.0/layer:/opt  lambci/lambda:provided  index.aplHandler '{\"some\": \"event\"}'
