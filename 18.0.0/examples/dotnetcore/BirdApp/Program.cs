a89822a94425a26d65bd97af36d55cd38988bab9
Marko Vranic <mvranic@yahoo.com>
2020-02-25 15:00:52 +0100

    Example tamplate.

---
+++ 18.0.0/examples/dotnetcore/BirdApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BirdApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
