d18d98b37ba361994837a6ae3adc605700e6122c
Marko Vranic <mvranic@yahoo.com>
2020-03-15 17:38:27 +0100

    APL 18.0 base (only 36mb zip).

--- 18.0.0/build.sh
+++ 18.0.0/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 export APL_VERSION=18.0.0
+export APL_MAYOR_VERSION=18.0
+export DYALOG_ROOTH_PATH=mdyalog/${APL_MAYOR_VERSION}/64/unicode/
 
-rm ./layer.zip
+rm ./layer*.zip
 rm -rf ./layer
 rm -rf ./output
 
@@ -18,6 +20,25 @@ cp -r source output/source
 
 cp -r dyalog/opt/mdyalog/ output/mdyalog
 
+
+# Remove not needed files for AWS lambda:
+ls -la output/${DYALOG_ROOTH_PATH}
+
+rm -rf output/${DYALOG_ROOTH_PATH}help
+rm -rf output/${DYALOG_ROOTH_PATH}xflib
+rm -rf output/${DYALOG_ROOTH_PATH}xfsrc
+rm -rf output/${DYALOG_ROOTH_PATH}Samples
+rm -rf output/${DYALOG_ROOTH_PATH}PublicCACerts
+rm -rf output/${DYALOG_ROOTH_PATH}TestCertificates
+
+rm -f output/${DYALOG_ROOTH_PATH}lib/libcef.so
+rm -f output/${DYALOG_ROOTH_PATH}lib/libcef.so
+rm -f output/${DYALOG_ROOTH_PATH}lib/cef*.pak
+rm -f output/${DYALOG_ROOTH_PATH}chrome-sandbox
+rm -f output/${DYALOG_ROOTH_PATH}libEGL*.so
+rm -f output/${DYALOG_ROOTH_PATH}libGLESv2.so
+ls -la output/${DYALOG_ROOTH_PATH}
+
 # Change mode for shell scripts:
 find ./output/ -type f -iname "*.sh" -exec chmod +x {} \;
 
@@ -28,10 +49,13 @@ find ./output/ -type f  \( -iname '*.sh' -o -iname '*.apl' -o -iname '*.dyalog' 
 
 export DOCKER_PORT=
 # To use docker in Windows:
-# export DOCKER_PORT=-H:2375
+export DOCKER_PORT=-H:2375
 
 docker ${DOCKER_PORT} build -t apl-provided-lambda-v${APL_VERSION} --build-arg APL_VERSION=${APL_VERSION} .
 docker ${DOCKER_PORT} run --rm apl-provided-lambda-v${APL_VERSION} cat /tmp/apl-v${APL_VERSION}.zip > ./layer.zip
+
+# max layer size in AWS is 50MB, therefore maybe more layers are needed.
+zipsplit -n 50000000 ./layer.zip
 
 # Buid end.
 
