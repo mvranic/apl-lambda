1acfcf8f69cf6e24dcbc6db9daabfb42f6a300ef
Marko Vranic <mvranic@yahoo.com>
2020-05-26 16:06:33 +0200

    Update pwd for powershell.

--- 18.0.0/test.sh
+++ 18.0.0/test.sh
@@ -11,5 +11,5 @@ rm -f lambda.zip && zip -qyr lambda.zip index
 docker run --rm -v "$PWD":/var/task -v "$PWD"/../layer:/opt lambci/lambda:provided index.aplHandler
 
 # powersehll:
-# docker run  --rm -v C:\apl-lambda\18.0.0\test:/var/task -v C:\apl-lambda\18.0.0/layer:/opt  lambci/lambda:provided  index.aplHandler
-# docker run  --rm -v C:\apl-lambda\18.0.0\test:/var/task -v C:\apl-lambda\18.0.0/layer:/opt  lambci/lambda:provided  index.aplHandler '{\"some\": \"event\"}'
+# docker run  --rm -v C:\apl-lambda\18.0.0\test:/var/task -v "$(pwd)/layer:/opt"  lambci/lambda:provided  index.aplHandler
+# docker run  --rm -v C:\apl-lambda\18.0.0\test:/var/task -v "$(pwd)/layer:/opt"  lambci/lambda:provided  index.aplHandler '{\"some\": \"event\"}'
